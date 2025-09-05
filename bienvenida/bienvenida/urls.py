79fa4512e1fb4508937da7b6a2d774475a74d461
andy-ancco <andy.coloma@inacapmail.cl>
2025-09-05 16:42:21 -0400

    feat: django parte 2

--- bienvenida/bienvenida/urls.py
+++ bienvenida/bienvenida/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inicio/', views.mostrar_bienvenida, name= 'Inicio'),
-    path('productos/', views.lista_productos, name='Productos')
-
+    path('productos/', views.lista_productos, name='Productos'),
+    path('inventario/', include('inventario.urls'))
 ]
